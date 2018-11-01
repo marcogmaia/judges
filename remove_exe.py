77108b15071540240ff2530ddd1282aaee101f6f
Marco G. Maia <marcogmaia@gmail.com>
2018-10-31 21:56:00 -0300

    added script to delete all *.exe files

---
+++ remove_exe.py
@@ -0,0 +1,13 @@
+import os
+
+folderToExclude = 'd:/marco/programming/cpp/judges'
+
+if __name__ == '__main__':
+    # Will traverse the current folder downwards looking for a *.exe to exclude
+    if os.path.dirname(__file__) == folderToExclude:
+        for root, dirs, files in os.walk('.'):
+            for file in files:
+                if file.endswith(".exe"):
+                    fileToExclude = os.path.join(root, file)
+                    os.remove(fileToExclude)
+                    print(f'{file} removed')
