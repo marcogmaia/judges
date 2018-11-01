ea52320f600debc266fa3b80206092ecd4d38c61
Marco G. Maia <marcogmaia@gmail.com>
2018-11-01 00:35:39 -0300

    455a

--- codeforces/455a.cpp
+++ codeforces/455a.cpp
@@ -5,9 +5,11 @@
 int main(int argc, char const *argv[]) {
     long long n;
     std::cin >> n;
+    // new() initialize all elements set to 0
     long long *cnt = new long long[NMAX]();
     long long *dp  = new long long[NMAX]();
 
+    // count how many times each number appear
     while(n--) {
         long long i;
         std::cin >> i;
@@ -18,6 +20,8 @@ int main(int argc, char const *argv[]) {
     dp[1]             = cnt[1];
     long long maximum = -1;
     for(long long i = 2; i < NMAX; ++i) {
+        // see if the previous has more points than the actual calculation
+        // and select the maximum between them
         dp[i]   = std::max(dp[i - 1], dp[i - 2] + (cnt[i] * i));
         maximum = std::max(dp[i], maximum);
     }
