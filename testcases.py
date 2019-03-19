ea899b2cb74bc405674cfbdcfa2e751ba8e45d2b
Marco G. Maia <marcogmaia@gmail.com>
2019-03-18 21:35:21 -0300

    .

--- testcases.py
+++ testcases.py
@@ -6,34 +6,30 @@ import subprocess
 import sys
 import io
 
+
+def timeit(decorated_funtion):
+    """
+    Outputs the time tha a function takes to run.
+    """
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        decorated_funtion(*args, **kwargs)
+        t2 = time.time()
+        print(f'Total running time = {t2-t1}')
+    return wrapper
+
+@timeit
+def main():
+    program_name = sys.argv[1]
+    input_file = sys.argv[2]
+
+    with open(input_file, 'r', encoding='utf8') as f:
+        for line in f:
+            proc = subprocess.Popen(program_name, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+            lineAsFile = io.StringIO(line)
+            output, err = proc.communicate(lineAsFile.read())
+            print(output, end='')
+
+
 if __name__ == '__main__':
-
-    program_name = sys.argv[1]  # program name
-    input_file = sys.argv[2]  # testCases file name
-
-    def timeit(decorated_function):
-        """Outputs the time that
-        a function takes to run."""
-
-        def wrapper(*args, **kwargs):
-            t1 = time.time()
-            decorated_function(*args, **kwargs)
-            t2 = time.time()
-            print("Total running time=", t2 - t1)
-
-        return wrapper
-
-    @timeit
-    def run():
-        with open(input_file, 'r', encoding='utf8') as f:
-            for line in f:
-                line_as_file = io.StringIO(line)
-                process = subprocess.Popen(
-                    program_name,
-                    stdin=subprocess.PIPE,
-                    stdout=subprocess.PIPE,
-                    universal_newlines=True)
-                output, output_err = process.communicate(line_as_file.read())
-                print(output, end='')
-
-    run()
+    main()
